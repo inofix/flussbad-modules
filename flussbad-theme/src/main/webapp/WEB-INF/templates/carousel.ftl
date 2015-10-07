0d652b63f0b761ba388dbc6b0fc7467a52374ff5
Michael Lustenberger <mic@inofix.ch>
2015-10-07 23:43:31 +0200

    YUI ScrollView Lazy Image Loader:
    Implemented a lazy image URL loader for the scrollview and added a stub
    for lazy loading of images in general with YUI ImageLoader.

--- flussbad-theme/src/main/webapp/WEB-INF/templates/carousel.ftl
+++ flussbad-theme/src/main/webapp/WEB-INF/templates/carousel.ftl
@@ -1,19 +1,20 @@
 <#--
     Carousel template: Format the Carousel structure
-    
+
     Created:    2015-08-28 17:51 by Christian Berndt
     Modified:   2015-09-28 11:54 by Christian Berndt
     Version:    0.9.4
-    
-    Please note: Although this template is stored in the 
-    site's context it's source is managed via git. Whenever you 
-    change the template online make sure that you commit your 
+
+    Please note: Although this template is stored in the
+    site's context it's source is managed via git. Whenever you
+    change the template online make sure that you commit your
     changes to the flussbad-modules repo, too.
 -->
 <div class="slide-show">
     <div id="scrollview-container">
       <div class="yui3-scrollview-loading carousel scrollview-content">
             <#if image.getSiblings()?has_content>
+                <#assign i = 0 >
                 <#list image.getSiblings() as cur_image>
                     <div class="item">
                         <!--
@@ -21,8 +22,13 @@
                             <h1>${cur_image.caption.getData()}</h1>
                         </div>
                         -->
-                        <img src="${cur_image.getData()}" title="${cur_image.caption.getData()}">
+                        <#if i gt 0>
+                            <img data-src="${cur_image.getData()}" title="${cur_image.caption.getData()}">
+                        <#else>
+                            <img src="${cur_image.getData()}" title="${cur_image.caption.getData()}">
+                        </#if>
                     </div>
+                    <#assign i = i+1>
                 </#list>
             </#if>
         </div>
--- flussbad-theme/src/main/webapp/js/main.js
+++ flussbad-theme/src/main/webapp/js/main.js
@@ -1,42 +1,42 @@
 /**
  * Scripts required by the flussbad-theme.
- * 
- * Created: 	2015-09-02 22:31 by Christian Berndt
- * Modified:	2015-10-06 11:48 by Christian Berndt
- * Version: 	1.1.1
+ *
+ * Created:     2015-09-02 22:31 by Christian Berndt
+ * Modified:    2015-10-06 11:48 by Christian Berndt
+ * Version:     1.1.1
  */
 
 /**
- * js for the main navigation, from 
+ * js for the main navigation, from
  * https://gist.github.com/randombrad/b5ffb95519eee2c56d87
  */
 AUI().ready(
 'liferay-hudcrumbs', 'liferay-navigation-interaction', 'liferay-sign-in-modal',
-	function(A) {
-		var navigation = A.one('#navigation');
+    function(A) {
+        var navigation = A.one('#navigation');
 
-		if (navigation) {
+        if (navigation) {
 
-			navigation.plug(Liferay.NavigationInteraction);
-			var menu_toggle = navigation.one('#nav_toggle');
-			
+            navigation.plug(Liferay.NavigationInteraction);
+            var menu_toggle = navigation.one('#nav_toggle');
+
             menu_toggle.on('click', function(event){
                 navigation.one('.collapse.nav-collapse').toggleClass('open');
             });
-		}
+        }
 
-		var siteBreadcrumbs = A.one('#breadcrumbs');
-		
-		if (siteBreadcrumbs) {
-			siteBreadcrumbs.plug(A.Hudcrumbs);
-		}
+        var siteBreadcrumbs = A.one('#breadcrumbs');
 
-		var signIn = A.one('li.sign-in a');
+        if (siteBreadcrumbs) {
+            siteBreadcrumbs.plug(A.Hudcrumbs);
+        }
 
-		if (signIn && signIn.getData('redirect') !== 'true') {
-			signIn.plug(Liferay.SignInModal);
-		}
-	}
+        var signIn = A.one('li.sign-in a');
+
+        if (signIn && signIn.getData('redirect') !== 'true') {
+            signIn.plug(Liferay.SignInModal);
+        }
+    }
 );
 
 /**
@@ -44,80 +44,80 @@ AUI().ready(
  */
 
 AUI().ready('node', function(A) {
-    
+
     var toggle = A.one('.accordion-toggle');
-    
+
     var categories = A.one('.portlet-asset-categories-navigation');
-    var publisher = A.one('.default-publisher'); 
-    
+    var publisher = A.one('.default-publisher');
+
     if (toggle) {
-	    toggle.on('click', function(event) {
-	    	
-	    	if (categories) {
-	            categories.toggleClass('categories-closed');
-	            categories.removeClass('categories-auto-closed');    		
-	    	}
-	        
-	    	if (publisher) {
-	            publisher.toggleClass('categories-closed');
-	            publisher.removeClass('categories-auto-closed');    		
-	    	}
-	
-	    });
-	}
-}); 
+        toggle.on('click', function(event) {
+
+            if (categories) {
+                categories.toggleClass('categories-closed');
+                categories.removeClass('categories-auto-closed');
+            }
+
+            if (publisher) {
+                publisher.toggleClass('categories-closed');
+                publisher.removeClass('categories-auto-closed');
+            }
+
+        });
+    }
+});
 
 /**
  * Show hide the asset categories filter while scrolling.
  */
 AUI().ready('node', 'node-scroll-info', function(A) {
-	
-	var body = A.one('body');
-	
-	var categories = A.one('.portlet-asset-categories-navigation'); 
-    var publisher = A.one('.default-publisher'); 
-		
-	body.plug(A.Plugin.ScrollInfo);
-	
-	body.scrollInfo.set('scrollDelay', 0); 
-		
-	body.scrollInfo.on('scrollDown', function (e) {
-		
-		if (categories) {
-	        categories.addClass('categories-auto-closed');			
-		}
-		if (publisher) {
-	        publisher.addClass('categories-auto-closed');			
-		}
 
-	});
-	
-	body.scrollInfo.on('scrollUp', function (e) {
-		
-		if (categories) {
-	        categories.removeClass('categories-auto-closed');			
-		}
-		if (publisher) {
-	        publisher.removeClass('categories-auto-closed');			
-		}
+    var body = A.one('body');
 
-	});
+    var categories = A.one('.portlet-asset-categories-navigation');
+    var publisher = A.one('.default-publisher');
+
+    body.plug(A.Plugin.ScrollInfo);
+
+    body.scrollInfo.set('scrollDelay', 0);
+
+    body.scrollInfo.on('scrollDown', function (e) {
+
+        if (categories) {
+            categories.addClass('categories-auto-closed');
+        }
+        if (publisher) {
+            publisher.addClass('categories-auto-closed');
+        }
+
+    });
+
+    body.scrollInfo.on('scrollUp', function (e) {
+
+        if (categories) {
+            categories.removeClass('categories-auto-closed');
+        }
+        if (publisher) {
+            publisher.removeClass('categories-auto-closed');
+        }
+
+    });
 });
 
 /**
  * A fallback for 100vh configuration of keyvisual elements.
  */
 YUI().use('event', 'node', function(Y) {
-	
+
     var winHeight = Y.one("body").get("winHeight") + 'px';
 
     /** TODO: all or only the first ? */
-    Y.all('.keyvisual').setStyle('height', winHeight);  
+    Y.all('.keyvisual').setStyle('height', winHeight);
 
-    Y.on('resize', function() {       
+    Y.on('resize', function() {
         winHeight = Y.one("body").get("winHeight") + 'px';
-        Y.all('.keyvisual').setStyle('height', winHeight);        
-    });	
+        Y.all('.keyvisual').setStyle('height', winHeight);
+    });
 });
 
 
@@ -139,17 +139,17 @@ YUI().use('event', 'node', 'scrollview-base', 'scrollview-paginator', function(Y
 
     var winWidth = Y.one("body").get("winWidth") + 'px';
 
-    Y.all('.carousel img').setStyle('width', winWidth);  
-    Y.all('.carousel .item').setStyle('width', winWidth); 
+    Y.all('.carousel img').setStyle('width', winWidth);
+    Y.all('.carousel .item').setStyle('width', winWidth);
 
     Y.on('resize', function() {
-        
+
       winWidth = Y.one("body").get("winWidth") + 'px';
 
-      Y.all('.carousel img').setStyle('width', winWidth);  
+      Y.all('.carousel img').setStyle('width', winWidth);
       Y.all('.carousel .item').setStyle('width', winWidth);
-      scrollView.set('width', winWidth); 
-      scrollView.render(); 
+      scrollView.set('width', winWidth);
+      scrollView.render();
     });
 
     scrollView.plug(Y.Plugin.ScrollViewPaginator, {
@@ -172,43 +172,58 @@ YUI().use('event', 'node', 'scrollview-base', 'scrollview-paginator', function(Y
     content.delegate("mousedown", function(e) {
         e.preventDefault();
     }, "img");
-    
+
+    // lazyLoad the image Url on demand only
+    function loadImgUrl(img) {
+//        console.log(img.get("id"));
+        if ( !img.getAttribute("src") && img.getAttribute("data-src") ) {
+            img.setAttribute("src", img.getAttribute("data-src"));
+        }
+    }
+
+    var imageArray = scrollView.get("srcNode").get("children").get(0);
+
     var total = scrollView.pages.get('total');
 
-    var nextControl = Y.one('.right.carousel-control'); 
-    
+    var nextControl = Y.one('.right.carousel-control');
+
     if (nextControl) {
-        
+
         nextControl.on('click', function(e){
-          
-          var idx = scrollView.pages.get('index'); 
-          var target = idx + 1; 
-          
+
+          var idx = scrollView.pages.get('index');
+          var target = idx + 1;
+
           if (target < total) {
-            scrollView.pages.set('index', target); 
+            scrollView.pages.set('index', target);
+            // see if we must lazyLoad the Url
+            loadImgUrl(imageArray[target].get("firstElementChild"));
           } else {
             // TODO: improve the flick behaviour at the ends
-            // scrollView.pages.set('index', total -1); 
-            scrollView.pages.scrollTo(0, 0.3, 'easing');           } 
-        });        
+            // scrollView.pages.set('index', total -1);
+            scrollView.pages.scrollTo(0, 0.3, 'easing');           }
+        });
     }
-   
+
     var prevControl = Y.one('.left.carousel-control')
-    
+
     if (prevControl) {
-      
+
           prevControl.on('click', function(e){
 
-          var idx = scrollView.pages.get('index'); 
-          var target = idx - 1; 
+          var idx = scrollView.pages.get('index');
+          var target = idx - 1;
 
           if (target < 0) {
             // TODO: improve the flick behaviour at the ends
-            // scrollView.pages.set('index', total -1); 
-            scrollView.pages.scrollTo(total - 1, 0.3, 'easing'); 
+            // scrollView.pages.set('index', total -1);
+            target = total - 1;
+            scrollView.pages.scrollTo(target, 0.3, 'easing');
           } else {
-            scrollView.pages.set('index', target); 
-          } 
+            scrollView.pages.set('index', target);
+          }
+            // see if we must lazyLoad the Url
+            loadImgUrl(imageArray[target].get("firstElementChild"));
         });
     }
 
@@ -219,71 +234,71 @@ YUI().use('event', 'node', 'scrollview-base', 'scrollview-paginator', function(Y
  * Show / hide the language-portlet included in #navigation
  */
 YUI().use(
-	'aui-popover','widget-anim',
-	function(Y) {
-	                            
-	  var trigger = Y.one('#langPopover');
-	  
-	      var popover = new Y.Popover(
-	        {
-	          align: {
-	            node: trigger,
-	            points:[Y.WidgetPositionAlign.TC, Y.WidgetPositionAlign.BL]
-	          },
-	          plugins : [ Y.Plugin.WidgetAnim ],
-	          position: 'bottom',
-	      visible: false,
-	      zIndex : 100
-	    }
-	  ).render();
-	  
-	  var portlet = Y.one('#p_p_id_82_');
-	  
-	  popover.set("bodyContent", portlet);
-	  
-	  popover.get('boundingBox').on('clickoutside', function() {
-	       popover.set('visible', false); 
+    'aui-popover','widget-anim',
+    function(Y) {
+
+      var trigger = Y.one('#langPopover');
+
+          var popover = new Y.Popover(
+            {
+              align: {
+                node: trigger,
+                points:[Y.WidgetPositionAlign.TC, Y.WidgetPositionAlign.BL]
+              },
+              plugins : [ Y.Plugin.WidgetAnim ],
+              position: 'bottom',
+          visible: false,
+          zIndex : 100
+        }
+      ).render();
+
+      var portlet = Y.one('#p_p_id_82_');
+
+      popover.set("bodyContent", portlet);
+
+      popover.get('boundingBox').on('clickoutside', function() {
+           popover.set('visible', false);
       });
-	  
-	  if (trigger) {
+
+      if (trigger) {
           trigger.on('click', function(e) {
-    	      popover.set('visible', !popover.get('visible'));
-    	          e.stopPropagation();   
+              popover.set('visible', !popover.get('visible'));
+                  e.stopPropagation();
           });
-	  }
-	}
+      }
+    }
 );
 
-/** 
+/**
  * Affix with offset for the main navigation on the start page
  */
 YUI().use(
     'aui-affix',
     function(Y) {
-    	
-        var startPageNavigation = Y.one('.start-page #navigation'); 
+
+        var startPageNavigation = Y.one('.start-page #navigation');
 
         if (startPageNavigation) {
-	        new Y.Affix(
-	        {
-	          target: '.start-page #navigation',
-	          offsetTop: 200
-	        });
+            new Y.Affix(
+            {
+              target: '.start-page #navigation',
+              offsetTop: 200
+            });
         }
     }
 );
 
-/** 
+/**
  * Affix the categories navigation below the main navigation
  */
 YUI().use(
     'aui-affix', 'node',
     function(Y) {
-        
+
         var portletAssetCategories = Y.one('.portlet-asset-categories-navigation.head-categories')
-        
+
         if (portletAssetCategories) {
-                        
+
             new Y.Affix(
             {
               target: '.portlet-asset-categories-navigation.head-categories',
@@ -293,17 +308,17 @@ YUI().use(
     }
 );
 
-/** 
+/**
  * Affix the project categories navigation below the main navigation
  */
 YUI().use(
     'aui-affix', 'node',
     function(Y) {
-        
+
         var portletAssetCategories = Y.one('.portlet-asset-categories-navigation.project-categories')
-        
+
         if (portletAssetCategories) {
-                        
+
             new Y.Affix(
             {
               target: '.portlet-asset-categories-navigation.project-categories',
@@ -313,17 +328,17 @@ YUI().use(
     }
 );
 
-/** 
+/**
  * Affix the toc of stories below the main-navigation
  */
 YUI().use(
     'aui-affix', 'node',
     function(Y) {
-        
+
         var toc = Y.one('.toc')
-        
+
         if (toc) {
-                        
+
             new Y.Affix(
             {
               target: '.toc',
@@ -334,22 +349,46 @@ YUI().use(
 );
 
 /**
- * Create one scrollspy per page 
+ * Create one scrollspy per page
  */
 YUI().use(
     'aui-scrollspy',
     function(Y) {
-        
-        var toc = Y.one('.toc ul'); 
-        
+
+        var toc = Y.one('.toc ul');
+
         if (toc) {
-            
-            console.log('has toc'); 
-            
+
+            console.log('has toc');
+
             new Y.Scrollspy({
                 offset: 120, /** height of navigation + margin-top of content */
                 target: '.toc ul'
             });
-        } 
+        }
     }
 );
+// TODO: needs IDs in DOM (even with AUI().ready())
+AUI().ready('imageloader', function (Y) {
+    var imageGroup = new Y.ImgLoadGroup({ name: 'foldGroup', foldDistance: 2 });
+//    imageGroup.addTrigger('.item', 'mouseover');
+
+//TODO: enable    var images = document.getElementsByTagName("img");
+//TODO: decide on going for CSS-Classes or elements: Y.all('img')
+    var images = [];
+
+//    console.log("imageloader: " + images.length);
+    for (var i = 0; i < images.length; i++)
+    {
+        if ( images[i].getAttribute("data-src") )
+        {
+//            console.log(images[i].get("title"));
+//            console.log(images[i].get("id"));
+            imageGroup.registerImage({
+                domId: images[i].getAttribute("id"),
+                srcUrl: images[i].getAttribute("data-src")
+            });
+        }
+    }
+});
+
